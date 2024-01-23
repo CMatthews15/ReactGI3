37847f6e126f15c7d44d69f8901ba453111347df
cmatthews15 <chastitydmatthews@gmail.com>
2024-01-23 09:39:45 -0500

    finish

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './styles.css'
+import Counter from './components/Counter'
+import SimilarMovies from './components/SimilarMovies';
+import ToDo from './components/ToDo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SimilarMovies /> */}
+      {/* <ToDo /> */}
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
